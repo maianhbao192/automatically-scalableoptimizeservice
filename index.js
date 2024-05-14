78fe8faf036515181cceb7d2c661dfbba09fcd24
maianhbao192 <ronapeahenpun0suy@hotmail.com>
2024-05-14 21:38:46 +0700

    init

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function numSquares(n) {
-  const dp = new Array(n + 1).fill(Infinity);
-  dp[0] = 0;
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j * j <= i; j++) {
-      dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
-    }
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
   }
-  return dp[n];
+  return false;
 }
