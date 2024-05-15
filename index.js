9f86fd79f2a3cf9fbc6f5a932a10ea251412c972
maianhbao192 <ronapeahenpun0suy@hotmail.com>
2024-05-15 11:05:14 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-function hasCycle(head) {
-  let slow = head;
-  let fast = head;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) return true;
-  }
-  return false;
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
 }
